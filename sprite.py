e1bba824247b444fac7d8cc001f8fdc5f44d236a
symon <symon@lenovo.com>
2023-03-02 10:13:14 -0400

    Shuffle and Reset buttons UI

--- sprite.py
+++ sprite.py
@@ -39,3 +39,31 @@ class Tile(pygame.sprite.Sprite):
         return self.rect.y- TILESIZE >=0
     def down(self):
         return self.rect.y + TILESIZE < GAME_SIZE * TILESIZE
+
+class UIElement:
+    def __init__(self,x,y,text):
+        self.x, self.y = x, y
+        self.text = text
+    def draw(self, screen):
+        font = pygame.font.SysFont("Consolas", 30)
+        text = font.render(self.text,True,WHITE)
+        screen.blit(text,(self.x,self.y))
+
+class Button:
+    def __init__(self,x,y,width,height,text,color,text_color):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+        self.text = text
+        self.color, self.text_color = color, text_color
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("Consolas", 30)
+        text = font.render(self.text,True,self.text_color)
+        self.font_size = font.size(self.text)
+        draw_x = self.x + (self.width / 2) - self.font_size[0]/2
+        draw_y = self.y + (self.height / 2) - self.font_size[1]/2
+        screen.blit(text, (draw_x, draw_y))
+
+    def click(self,mouse_x, mouse_y):
+        return self.x <= mouse_x <=self.x + self.width and self.y <= mouse_y <= self.y + self.height
